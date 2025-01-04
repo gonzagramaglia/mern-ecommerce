d69c86015a7e80f42d811e92ec5e21901f058dd5
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-04 14:53:54 -0300

    frontend: Navbar. styling the component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,41 @@
 import { assets } from "../../assets/assets";
 import "./Navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("menu");
+
   return (
     <div className="navbar">
       <img src={assets.logo} alt="logo" className="logo" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search icon" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign In</button>
+      </div>
     </div>
   );
 };
