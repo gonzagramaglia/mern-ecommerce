92c2b20214dc38c7db461fb5a5fbc66931e90d09
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-05 03:40:24 -0300

    frontend: React Router DOM >>> BrowserRouter >> Routes > Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import Navbar from "./components/Navbar/Navbar";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
