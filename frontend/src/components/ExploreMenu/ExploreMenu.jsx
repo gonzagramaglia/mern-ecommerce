0d5641b32231c1bdfb05c5b3554a7e65ca5a1b57
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-05 06:05:41 -0300

    frontend: ExploreMenu section. selectiong and filtering menu items

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,7 @@
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <>
       <div className="explore-menu" id="explore-menu">
@@ -11,10 +11,36 @@ const ExploreMenu = () => {
         </p>
         <div className="explore-menu-list">
           {menu_list.map((list_item, index) => {
+            const menu_name = list_item.menu_name;
+            const menu_image = list_item.menu_image;
             return (
-              <div key={index} className="explore-menu-list-item">
-                <img src={list_item.menu_image} alt={list_item.menu_name} />
-                <p>{list_item.menu_name}</p>
+              <div
+                onClick={() =>
+                  setCategory((prev) =>
+                    prev === menu_name ? "All" : menu_name
+                  )
+                }
+                key={index}
+                className="explore-menu-list-item"
+              >
+                <img
+                  className={
+                    (category === menu_name) | (category === "All")
+                      ? "active"
+                      : ""
+                  }
+                  src={menu_image}
+                  alt={menu_name}
+                />
+                <p
+                  className={
+                    (category === menu_name) | (category === "All")
+                      ? "active"
+                      : ""
+                  }
+                >
+                  {menu_name}
+                </p>
               </div>
             );
           })}
