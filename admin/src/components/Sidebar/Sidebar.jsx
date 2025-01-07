7acfe85ece7ea4493b37c769ac89a9ac2a72d0f9
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2025-01-07 17:55:25 -0300

    frontend: Admin panel setup

---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import "./Sidebar.css";
+import { assets } from "../../assets/assets.js";
+import { NavLink } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="sidebar">
+        <div className="sidebar-options">
+          <NavLink to="/add" className="sidebar-option">
+            <img src={assets.add_icon} alt="add items" />
+            <p>Add Items</p>
+          </NavLink>
+          <NavLink to="/list" className="sidebar-option">
+            <img src={assets.order_icon} alt="list items" />
+            <p>List Items</p>
+          </NavLink>
+          <NavLink to="/orders" className="sidebar-option">
+            <img src={assets.order_icon} alt="orders" />
+            <p>Orders</p>
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
